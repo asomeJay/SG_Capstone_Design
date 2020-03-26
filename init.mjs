fb2d96b4b1e3c7c7137a17c7fb287d4f6c056780
asomeJay <museum114@naver.com>
2020-03-26 18:44:14 +0000

    Naver CLoud API를 이용해서 지도를 더욱 정교하게 만듬

--- init.mjs
+++ init.mjs
@@ -6,14 +6,15 @@ import request from 'request-promise';
 import bodyParser from 'body-parser';
 import moment from 'moment';
 import timezone from 'moment-timezone';
-import geoip from "geoip-lite";
-
+import { user_location_info } from './mask.mjs';
+import axios from 'axios';
+import async from 'async'
 
 const app = express();
 moment.tz.setDefault("Asia/Seoul");
 const port = 8000;
-var geo;
-var user_ip;
+
+
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -33,7 +34,7 @@ app.get('/', (req, res) => {
     res.send(t)
     console.log(t);
 });
-//app.get('/mask', mask());
+
 app.post('/', (req, res) => {
     var ret = {
         "email": "museum114@naver.com",
@@ -49,47 +50,46 @@ app.post('/', (req, res) => {
 
 });
 
-app.get('/mask', async (req, res) => {
-    user_ip = requestip.getClientIp(req).slice(7);
-    geo = geoip.lookup(user_ip);
-    const mask_info = await find_nearest_store(geo.ll[0], geo.ll[1]);
-
-    var result = `${moment().format("YYYY-MM-DD HH:mm:ss")}` + "<br>";
-    result += `당신은 ${geo.city} { 위도 : ${geo.ll[0]}, 경도 : ${geo.ll[1]} }에 위치해있습니다<br>`;
-    result += `가장 가까운 마스크 판매점은 <br>`;
-    result += `<h2>${mask_info.mask_addr}</h2>`;
-    result += `<b><font size = "5.5">${mask_info.mask_name} </font></b> 입니다<br><br>`;
-    result += "<b><font size = '5.5'>";
-    if (mask_info.remain_stat >= 100) 
-        result += `100개 이상 남았습니다. <br>`;    
-    else if(mask_info.remain_stat >= 30)
-        result += `30개 ~ 99개 남았습니다.<br>`;    
-    else if (mask_info.remain_stat >= 2)
-        result += `2개 ~ 29개 남았습니다.<br>`;    
-    else
-        result += `0 ~ 1개 남았습니다. <br>`;       
-    result += "</font></b>";
-
-    res.send(result);
+app.get('/mask', (req, res) => {
+    user_location_info(req, res)
 });
 
-async function find_nearest_store(lat, lon) {
-    var mask_name, mask_addr, mask_remain_stat;
+export async function find_nearest_store(user_location_info, user_ip, res) {
+    var lat = user_location_info.geoLocation.lat;
+    var lon = user_location_info.geoLocation.long;
+    var user_geo = user_location_info.geoLocation;
+    var mask_name, mask_addr, remain_stat;
+
+    var geo_city = user_geo.r1 + " " + user_geo.r2 + " " + user_geo.r3; 
     var request_option = {
         url: mask_url + `?lat=${lat.toString()}` + `&lng=${lon.toString()}`+ "&m=2000",
         method: 'GET',
         json: true
     };
 
-    console.log(moment().format("YYYY-MM-DD HH:mm:ss"), geo.city, lat, lon, user_ip);
+    console.log(moment().format("YYYY-MM-DD HH:mm:ss"), geo_city, user_ip , lat, lon);
     
     await request(request_option, (_, __, body) => {
         mask_name = body.stores[0].name;
         mask_addr = body.stores[0].addr;
-        mask_remain_stat = body.stores[0].remain_stat;
+        remain_stat = body.stores[0].remain_stat;
     });
-    
-    return {mask_name, mask_addr, mask_remain_stat};
+    var result = `${moment().format("YYYY-MM-DD HH:mm:ss")}` + "<br>";
+    const cord = `{ 위도: ${ lat }, 경도: ${ lon }}`;
+    result += `당신은 ${geo_city}에 위치해있습니다<br>`;
+    result += `가장 가까운 마스크 판매점은 <br>`;
+    result += `<h2>${mask_addr}</h2>`;
+    result += `<b><font size = "5.5">${mask_name} </font></b> 입니다<br><br>`;
+    result += "<b><font size = '5.5'>";
+    if (remain_stat >= 100) 
+        result += `100개 이상 남았습니다. <br>`;    
+    else if(remain_stat >= 30)
+        result += `30개 ~ 99개 남았습니다.<br>`;    
+    else if (remain_stat >= 2)
+        result += `2개 ~ 29개 남았습니다.<br>`;    
+    else
+        result += `0 ~ 1개 남았습니다. <br>`;       
+    result += "</font></b>";
+    res.send(result);
 }
-
 export default app;
--- mask.mjs
+++ mask.mjs
@@ -1,21 +1,66 @@
-import express from "express";
+import dot from 'dotenv'
+import axios from 'axios'
+import CryptoJS from 'crypto-js'
+import requestip from 'request-ip';
+import { find_nearest_store } from './init.mjs';
 
-const globalRouter = express.Router();
+dot.config();
 
-export const location = (req, res) => {
-    console.log(res);
-    if (document.navigator.geolocation) {
-        console.log("IN");
-        var new_div = document.createElement("div");
-        navigator.geolocation.getCurrentPosition(function (pos) {
-            new_div.innerHTML(pos.coords);
-            console.log(pos.cords);
-            document.body.appendChild(new_div);
-        })
+const access_key = process.env.access_key;
+const secret_key = process.env.secret_key;
+
+const requestMethod = "GET";
+const hostName = 'https://geolocation.apigw.ntruss.com'
+const requestUrl= '/geolocation/v2/geoLocation'
+
+const timeStamp = Math.floor(+new Date).toString();
+
+export const user_location_info = (req, res) => {
+    const user_ip = requestip.getClientIp(req).slice(7);
+    const sortedSet = {};
+    sortedSet["ip"] = user_ip;
+    sortedSet["ext"] = "t";
+    sortedSet["responseFormatType"] = "json";
+  
+    let queryString = Object.keys(sortedSet).reduce((prev, curr) => {
+        return prev + curr + '=' + sortedSet[curr] + '&';
+    }, "");
+  
+    queryString = queryString.substr(0, queryString.length - 1);
+  
+    const baseString = requestUrl + "?" + queryString;
+    const signature = makeSignature(secret_key, requestMethod, baseString, timeStamp, access_key);
+  
+    const config = {
+        headers: {
+            'x-ncp-apigw-timestamp': timeStamp,
+            'x-ncp-iam-access-key': access_key,
+            'x-ncp-apigw-signature-v2': signature
+        }
     }
-    res.render('./views/mask.html');
+  
+    axios.get(`${hostName}${baseString}`, config)
+        .then(response => {
+            const result = find_nearest_store(response.data, user_ip, res);
+        })
+        .catch(error => { console.log(error.response.data); })
+    
+};
+
+function makeSignature(secretKey, method, baseString, timestamp, accessKey) {
+    const space = " ";
+    const newLine = "\n";
+    let hmac = CryptoJS.algo.HMAC.create(CryptoJS.algo.SHA256, secretKey);
+
+    hmac.update(method);
+    hmac.update(space);
+    hmac.update(baseString);
+    hmac.update(newLine);
+    hmac.update(timestamp);
+    hmac.update(newLine);
+    hmac.update(accessKey);
+    const hash = hmac.finalize();
+
+    return hash.toString(CryptoJS.enc.Base64);
 }
 
-globalRouter.get('/mask', location);
-
-export default globalRouter;
