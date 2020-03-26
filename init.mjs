56565d7362e428c70d3b966ebfec6ca3e4cc1407
asomeJay <museum114@naver.com>
2020-03-26 13:35:57 +0000

    MASK API 활용과 GET&POST

--- init.mjs
+++ init.mjs
@@ -4,16 +4,18 @@ import url from 'url';
 import requestip from 'request-ip';
 import request from 'request-promise';
 import bodyParser from 'body-parser';
+import moment from 'moment';
+import timezone from 'moment-timezone';
 
 const app = express();
-
+moment.tz.setDefault("Asia/Seoul");
 const port = 8000;
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var date_ob = new Date();
+let date_ob = new Date();
 const mask_url = "https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/storesByGeo/json"
 // current date
 // adjust 0 before single digit date
@@ -48,7 +50,7 @@ app.get('/', (req, res) => {
     var ret = {
         "email": "museum114@naver.com",
         "stuno": "20150038",
-        "time": `${time_expression}`,
+        "time": `${moment().format("YYYY-MM-DD HH:mm:ss")}`,
         "ip": `${requestip.getClientIp(req).slice(7)}`
     }
     var t = Object.assign(ret, url_parts.query)
@@ -57,17 +59,17 @@ app.get('/', (req, res) => {
 });
 //app.get('/mask', mask());
 app.post('/', (req, res) => {
-    url_parts = url.parse(req.url, true);
-    var time_expression = year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds;
     var ret = {
         "email": "museum114@naver.com",
         "stuno": "20150038",
-        "time": `${time_expression}`,
+        "time": `${moment().format("YYYY-MM-DD HH:mm:ss")}`,
         "ip": `${requestip.getClientIp(req).slice(7)}`
     }
-
-    var t = Object.assign(ret, url_parts.query)
-    res.send(req.body);
+    var t = Object.assign(ret, req.body);
+    console.log(t);
+    res.setHeader("200", { "Content-Type": "text/plain" });
+    res.send(t);
+    res.end(JSON.stringify(t, null, 2));
 
 });
 
@@ -85,7 +87,7 @@ app.get('/mask', async (req, res) => {
         console.log("?");
         mask_cnt = body.stores[0];
     });
-    var result = time_expression + " " + mask_cnt.name +" 마스크가 ";
+    var result = `${moment().format("YYYY-MM-DD HH:mm:ss")}` + " " + mask_cnt.name +" 마스크가 ";
         result += mask_cnt.remain_stat + " 입니다.";
         console.log(result);
 
--- thingspeak_get.py
+++ thingspeak_get.py
@@ -1,8 +1,12 @@
-import requests
+import requests, json
 from random import *
 import time
-for i in range(20):
-    value = randint(0, 30)
-    requests.get("https://api.thingspeak.com/update?api_key=R62VDH4YIFYYP382&field1=" + str(value))
-    time.sleep(20)
+data = {'name1': 'jaewon', 'param2': 'value'}
+url = "http://ec2-54-81-185-130.compute-1.amazonaws.com:8000"
+res = requests.post(url, data = data).text
+print(res)
+#for i in range(20):
+ #   value = randint(0, 30)
+  #  requests.post("http://ec2-54-81-185-130.compute-1.amazonaws.com:8000/")
+   # time.sleep(20)
 
