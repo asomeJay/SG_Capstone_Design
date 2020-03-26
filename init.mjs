cbe7f71ba75ddceb0eb2f7c74591a0904297deae
asomeJay <museum114@naver.com>
2020-03-26 14:44:51 +0000

    Mask API 위치 파악 추가

--- init.mjs
+++ init.mjs
@@ -6,11 +6,13 @@ import request from 'request-promise';
 import bodyParser from 'body-parser';
 import moment from 'moment';
 import timezone from 'moment-timezone';
+import geoip from "geoip-lite";
+
 
 const app = express();
 moment.tz.setDefault("Asia/Seoul");
 const port = 8000;
-
+var geo;
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -47,22 +49,47 @@ app.post('/', (req, res) => {
 });
 
 app.get('/mask', async (req, res) => {
-    var mask_cnt = ":";
+    var user_ip = requestip.getClientIp(req).slice(7);
+    geo = geoip.lookup(user_ip);
+    const mask_info = await find_nearest_store(geo.ll[0], geo.ll[1]);
 
+    var result = `${moment().format("YYYY-MM-DD HH:mm:ss")}` + "<br>";
+    result += `당신은 ${geo.city} { 위도 : ${geo.ll[0]}, 경도 : ${geo.ll[1]} }에 위치해있습니다<br>`;
+    result += `가장 가까운 마스크 판매점은 <br>`;
+    result += `<h2>${mask_info.mask_addr}</h2>`;
+    result += `<b><font size = "5.5">${mask_info.mask_name} </font></b> 입니다<br><br>`;
+    result += "<b><font size = '5.5'>";
+    if (mask_info.remain_stat >= 100) 
+        result += `100개 이상 남았습니다. <br>`;    
+    else if(mask_info.remain_stat >= 30)
+        result += `30개 ~ 99개 남았습니다.<br>`;    
+    else if (mask_info.remain_stat >= 2)
+        result += `2개 ~ 29개 남았습니다.<br>`;    
+    else
+        result += `0 ~ 1개 남았습니다. <br>`;       
+    result += "</font></b>";
+
+    res.send(result);
+});
+
+async function find_nearest_store(lat, lon) {
+    var mask_name, mask_addr, mask_remain_stat;
     var request_option = {
-        url: mask_url,
+        url: mask_url + `?lat=${lat.toString()}` + `&lng=${lon.toString()}`+ "&m=2000",
         method: 'GET',
         json: true
     };
-    console.log("ENTER");
-    await request(request_option, (_, __, body) => {
-        mask_cnt = body.stores[0];
-    });
-    var result = `${moment().format("YYYY-MM-DD HH:mm:ss")}` + " " + mask_cnt.name +" 마스크가 ";
-        result += mask_cnt.remain_stat + " 입니다.";
-        console.log(result);
 
-        res.send(result);
-});
+    console.log("ENTER");
+    
+    await request(request_option, (_, __, body) => {
+        console.log("?");
+        mask_name = body.stores[0].name;
+        mask_addr = body.stores[0].addr;
+        mask_remain_stat = body.stores[0].remain_stat;
+    });
+    
+    return {mask_name, mask_addr, mask_remain_stat};
+}
 
 export default app;
